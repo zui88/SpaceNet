581c45ee6239dbad62714d1e2bc523b3ae82e4d9
Marco Burkhardt <mburkhardt6@gmail.com>
2026-02-08 20:52:44 +0100

    Refactor testbench-music.py: encapsulate signal generation and steering logic into ULASignalGenerator class.

--- testbench-music.py
+++ testbench-music.py
@@ -1,29 +1,48 @@
 import numpy as np
 
-rng = np.random.default_rng()
+class ULASignalGenerator:
 
-K_sources = 5
-N_antennas = 8
-N_samples = 10
-SNR_db = 20
-sigma2 = np.power(10, -SNR_db/10)
-theta = rng.uniform(-np.pi/2, np.pi/2, K_sources)
+    def __init__(self, n_antennas = 8):
+        self.rng = np.random.default_rng()
+        self.n_antennas = n_antennas
 
-##################################################
-# constructing the incident/detected signals
-# s_k,n = x_k,n + jy_k,n with x,y ~ N(0,1)
-##################################################
-s = np.sqrt(2) * (rng.standard_normal((K_sources, N_samples)) + 1j * rng.standard_normal((K_sources, N_samples)))
+    def get_steering(self, thetas):
+        ##################################################
+        # steering/mode vectors for "standard ULA"
+        # hypothesises Theta_i
+        # A.shape() = (antennas x sources)
+        ##################################################
+        antennas_idx = np.arange(self.n_antennas)[:, None]
+        A = np.exp(1j * np.pi * np.sin(thetas) * antennas_idx)  # broadcast sin with numbers of antennas
+        return A
 
-##################################################
-# steering/mode vectors for "standard ULA"
-# hypothesises Theta_i
-##################################################
-antennas_idx = np.arange(N_antennas)[:, None]
-A = np.exp(1j * np.pi * np.sin(theta) * antennas_idx) # broadcast sin with numbers of antennas
+    def get_incident_signals(self, k_sources, n_samples):
+        ##################################################
+        # constructing the incident signals
+        # s_k,n = x_k,n + jy_k,n with x,y ~ N(0,1)
+        # just random information to be sent (doesn't matter in this case)
+        ##################################################
+        x = y = self.rng.standard_normal((k_sources, n_samples))
+        s = 1/np.sqrt(2) * (x + 1j*y)
+        return s
 
-##################################################
-# sensed signal
-##################################################
-w = np.sqrt(sigma2 / 2) * (rng.standard_normal((N_antennas, N_samples)) + 1j * rng.standard_normal((N_antennas, N_samples)))
-r = A @ s + w
+    def generate(self, thetas, n_samples=10, snr_db=20):
+        ##################################################
+        # sensed signal
+        ##################################################
+        k_sources = len(thetas)
+        sigma2 = np.power(10, -snr_db/10)
+
+        w = np.sqrt(sigma2 / 2) * (self.rng.standard_normal((self.n_antennas, n_samples)) + 1j * self.rng.standard_normal((self.n_antennas, n_samples)))
+        A = self.get_steering(thetas)
+        s = self.get_incident_signals(k_sources, n_samples)
+        return A @ s + w
+
+
+gen = ULASignalGenerator()
+thetas = np.deg2rad([-20, 20, 40, 60])
+r  = gen.generate(thetas)
+
+R = (r @ r.conj().T) / len(r[0,:])
+eigs, vecs = np.linalg.eig(R)
+print(eigs)
