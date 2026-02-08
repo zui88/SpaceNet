90137b5049fc484863934111a6e21d92ae1e84e2
Marco Burkhardt <mburkhardt6@gmail.com>
2026-02-08 19:54:42 +0100

    Refactor testbench-music.py: remove unused imports, simplify signal generation and steering vector computation, and adjust noise scaling.

--- testbench-music.py
+++ testbench-music.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy as sp
 
 rng = np.random.default_rng()
 
@@ -9,25 +7,23 @@ N_antennas = 8
 N_samples = 10
 SNR_db = 20
 sigma2 = np.power(10, -SNR_db/10)
-theta = rng.random(K_sources)
+theta = rng.uniform(-np.pi/2, np.pi/2, K_sources)
 
 ##################################################
 # constructing the incident/detected signals
 # s_k,n = x_k,n + jy_k,n with x,y ~ N(0,1)
 ##################################################
-s = np.square(0.5) * (rng.standard_normal((K_sources, N_samples)) + 1j * rng.standard_normal((K_sources, N_samples)))
+s = np.sqrt(2) * (rng.standard_normal((K_sources, N_samples)) + 1j * rng.standard_normal((K_sources, N_samples)))
 
 ##################################################
 # steering/mode vectors for "standard ULA"
 # hypothesises Theta_i
 ##################################################
-antennas_iter = np.arange(N_antennas)[:, None]
-A = np.exp(1j * np.pi * np.sin(theta[:, None].T) * antennas_iter) # broadcast sin with numbers of antennas
+antennas_idx = np.arange(N_antennas)[:, None]
+A = np.exp(1j * np.pi * np.sin(theta) * antennas_idx) # broadcast sin with numbers of antennas
 
 ##################################################
 # sensed signal
 ##################################################
-w = np.sqrt(sigma2 * 0.5) * (rng.standard_normal((N_antennas, N_samples)) + 1j * rng.standard_normal((N_antennas, N_samples)))
-r = np.zeros((N_antennas,N_samples), dtype=np.complex128)
-for n in range(N_samples):
-    r[:,n:n+1] = A @ s[:, n:n + 1] + w[:, n:n + 1]
+w = np.sqrt(sigma2 / 2) * (rng.standard_normal((N_antennas, N_samples)) + 1j * rng.standard_normal((N_antennas, N_samples)))
+r = A @ s + w
